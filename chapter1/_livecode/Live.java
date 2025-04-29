0288111a5164781b9d4629489db4d3710297180c
DESKTOP-GR883KV\user <mirae3951@gmail.com>
2025-04-29 10:36:41 +0900

    live commint

--- chapter1/_livecode/Live.java
+++ chapter1/_livecode/Live.java
@@ -1,7 +1,9 @@
 package chapter1._livecode;
 
-public class Live {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.util.Scanner;
 
+public class Live {
+    public static void main(String[] args) throws IOException {
     }
 }
